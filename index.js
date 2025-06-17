87999e5af18e645d87058bc03d1b8aad75e7217d
Muhammad Hanif Abdillah <muhammadhanifa23@gmail.com>
2025-06-17 23:52:40 +0700

    node-red init

---
+++ index.js
@@ -0,0 +1,25 @@
+const http = require("http");
+const express = require("express");
+const RED = require("node-red");
+
+const app = express();
+const server = http.createServer(app);
+
+const settings = {
+  httpAdminRoot: "/red",
+  httpNodeRoot: "/api",
+  userDir: "./data",
+  flowFile: "flows.json",
+  functionGlobalContext: {},
+  port: process.env.PORT || 1880
+};
+
+RED.init(server, settings);
+app.use(settings.httpAdminRoot, RED.httpAdmin);
+app.use(settings.httpNodeRoot, RED.httpNode);
+
+server.listen(settings.port, () => {
+  console.log(`Node-RED is running on http://localhost:${settings.port}${settings.httpAdminRoot}`);
+});
+
+RED.start();
